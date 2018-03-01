f154b2829541a5ebf46fbfefbc1984c1b02a9ac4
shan <gsshanker10@gmail.com>
2018-03-01 15:46:14 +0530

    db credentials

--- wp-config.php
+++ wp-config.php
@@ -27,7 +27,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', $db);
+define('DB_NAME',"caxid");
 
 /** MySQL database username */
 define('DB_USER', "shan");
